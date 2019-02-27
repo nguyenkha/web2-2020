76e858e07ae3dca4ff6e0162fa97fc225babb8ad
Kha N. Do <donguyenkha@gmail.com>
2019-02-27 08:17:39 +0700

    Update part 2

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
-console.log('Hello world - LTWeb 2');
+// Load HTTP module
+var http = require("http");
+
+var port = process.env.PORT || 3000;
+
+// Create HTTP server and listen on port 8000 for requests
+http.createServer(function (request, response) {
+  console.log(request.h);
+
+  // Set the response HTTP header with HTTP status and Content type
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body "Hello World"
+  response.end('Hello World\n');
+}).listen(port);
+
+// Print URL for accessing server
+console.log(`Server running at http://127.0.0.1:${port}/`);
---
+++ simple/app.js
@@ -0,0 +1,49 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+
+http.createServer(function (request, response) {
+  console.log('request ', request.url);
+
+  var filePath = '.' + request.url;
+  if (filePath == './') {
+    filePath = './index.html';
+  }
+
+  var extname = String(path.extname(filePath)).toLowerCase();
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+  };
+
+  var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('./404.html', function (error, content) {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+        response.end();
+      }
+    }
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
+
+}).listen(port);
+console.log(`Server running at http://127.0.0.1:${port}/`);
